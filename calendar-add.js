6330ab49982845160bf267423b467d792e5db669
Riley <riley@dingdigital.co.uk>
2025-01-12 07:14:35 +0000

    fixes for google and ical formats

--- calendar-add.js
+++ calendar-add.js
@@ -6,15 +6,15 @@ class CalendarEntryCreator extends HTMLElement {
       <link rel="stylesheet" href="calendar-styles.css">
       <div class="calendar-add">
         <button class="calendar-add__button calendar-add__button--toggle" id="toggle-options">Add to Calendar</button>
-        <div class="calendar-add__options" style="display: none;">
+        <div class="calendar-add__options">
           <input type="text" class="calendar-add__input calendar-add__input--title" id="title" placeholder="Event Title">
           <input type="datetime-local" class="calendar-add__input calendar-add__input--start-time" id="start-time">
           <input type="datetime-local" class="calendar-add__input calendar-add__input--end-time" id="end-time">
           <input type="text" class="calendar-add__input calendar-add__input--location" id="location" placeholder="Location">
           <textarea class="calendar-add__textarea" id="description" placeholder="Description"></textarea>
-          <button class="calendar-add__button calendar-add__button--google" id="create-google">Create in Google Calendar</button>
-          <button class="calendar-add__button calendar-add__button--outlook" id="create-outlook">Create in Outlook Calendar</button>
-          <button class="calendar-add__button calendar-add__button--ical" id="create-ical">Create iCal File</button>
+          <button class="calendar-add__button calendar-add__button--google" id="create-google">Google Calendar</button>
+          <button class="calendar-add__button calendar-add__button--outlook" id="create-outlook">Outlook Calendar</button>
+          <button class="calendar-add__button calendar-add__button--ical" id="create-ical">Download iCal File</button>
         </div>
       </div>
     `;
@@ -27,6 +27,13 @@ class CalendarEntryCreator extends HTMLElement {
     this.outlookButton.addEventListener('click', () => this.createInOutlookCalendar());
     this.icalButton.addEventListener('click', () => this.createICalFile());
 
+    // Add event listener for the toggle button
+    this.shadowRoot.getElementById('toggle-options').addEventListener('click', () => {
+      this.shadowRoot.querySelector('.calendar-add__options').classList.toggle('show');
+    });
+  }
+
+  connectedCallback() {
     // Pull properties from attributes
     const eventName = this.getAttribute('event-name');
     const startTime = this.getAttribute('start-time');
@@ -43,18 +50,12 @@ class CalendarEntryCreator extends HTMLElement {
 
     // Check for the 'show-add-button' attribute
     if (this.hasAttribute('show-add-button')) {
-      this.shadowRoot.getElementById('toggle-options').style.display = 'flex'; // Show the button
-      this.shadowRoot.querySelector('.calendar-add__options').style.display = 'none'; // Hide the options
+      // add the show class to the options
+      this.shadowRoot.querySelector('.calendar-add__options').classList.remove('show');
     } else {
       this.shadowRoot.getElementById('toggle-options').style.display = 'none'; // Hide the button
-      this.shadowRoot.querySelector('.calendar-add__options').style.display = 'flex'; // Show the options
+      this.shadowRoot.querySelector('.calendar-add__options').classList.add('show'); // Show the options
     }
-
-    // Add event listener for the toggle button
-    this.shadowRoot.getElementById('toggle-options').addEventListener('click', () => {
-      const options = this.shadowRoot.querySelector('.calendar-add__options');
-      options.style.display = options.style.display === 'none' ? 'flex' : 'none'; // Toggle visibility
-    });
 
     // Check for the 'hide-inputs' attribute
     if (this.hasAttribute('hide-inputs')) {
@@ -72,12 +73,37 @@ class CalendarEntryCreator extends HTMLElement {
     const description = this.shadowRoot.getElementById('description').value;
     const location = this.shadowRoot.getElementById('location').value;
 
+    let formattedStartTime = '';
+    let formattedEndTime = '';
+
     // 2. Format dates to YYYYMMDDTHHMMSSZ
-    const formattedStartTime = new Date(startTime).toISOString().replace(/-|:|\.\d{3}/g, '');
-    const formattedEndTime = new Date(endTime).toISOString().replace(/-|:|\.\d{3}/g, '');
+    if (startTime) {
+      formattedStartTime = new Date(startTime).toISOString().replace(/-|:|\.\d{3}/g, '');
+    }
+    if (endTime) {
+      formattedEndTime = new Date(endTime).toISOString().replace(/-|:|\.\d{3}/g, '');
+    }
 
     // 3. Construct Google Calendar event URL
-    const url = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}&dates=${formattedStartTime}/${formattedEndTime}&details=${encodeURIComponent(description)}&location=${encodeURIComponent(location)}`;
+    let url = `https://calendar.google.com/calendar/render?action=TEMPLATE&text=${encodeURIComponent(title)}`;
+    if (formattedStartTime) {
+      url += `&dates=${formattedStartTime}`;
+    }
+    if (formattedEndTime) {
+      url += `/${formattedEndTime}`;
+    } else {
+      // start time plus one hour
+      const startDate = new Date(startTime);
+      startDate.setHours(startDate.getHours() + 1);
+      formattedEndTime = startDate.toISOString().replace(/-|:|\.\d{3}/g, '');
+      url += `/${formattedEndTime}`;
+    }
+    if (description) {
+      url += `&details=${encodeURIComponent(description)}`;
+    }
+    if (location) {
+      url += `&location=${encodeURIComponent(location)}`;
+    }
 
     // 4. Open the URL in a new window/tab
     window.open(url, '_blank');
@@ -107,7 +133,19 @@ class CalendarEntryCreator extends HTMLElement {
     const location = this.shadowRoot.getElementById('location').value;
 
     // 2. Generate iCal content
-    const iCalContent = `BEGIN:VCALENDAR\nVERSION:2.0\nBEGIN:VEVENT\nSUMMARY:${title}\nDTSTART:${startTime}\nDTEND:${endTime}\nDESCRIPTION:${description}\nLOCATION:${location}\nEND:VEVENT\nEND:VCALENDAR`;
+    const iCalContent = `BEGIN:VCALENDAR\r\n`
+      + `VERSION:2.0\r\n`
+      + `CALSCALE:GREGORIAN\r\n`
+      + `METHOD:PUBLISH\r\n`
+      + `BEGIN:VEVENT\r\n`
+      + `DTSTAMP:${new Date().toISOString().replace(/-|:|\.\d{3}/g, '').slice(0, 15)}Z\r\n`
+      + `SUMMARY:${title}\r\n`
+      + `DTSTART:${new Date(startTime).toISOString().replace(/-|:|\.\d{3}/g, '').slice(0, 15)}Z\r\n`
+      + `DTEND:${new Date(endTime).toISOString().replace(/-|:|\.\d{3}/g, '').slice(0, 15)}Z\r\n`
+      + `DESCRIPTION:${description.substring(0, 60)}...\r\n`
+      + `LOCATION:${location}\r\n`
+      + `END:VEVENT\r\n`
+      + `END:VCALENDAR\r\n`;
 
     // 3. Create a Blob with the iCal content
     const blob = new Blob([iCalContent], { type: 'text/calendar' });
@@ -115,7 +153,7 @@ class CalendarEntryCreator extends HTMLElement {
     // 4. Create a temporary URL for the Blob
     const url = URL.createObjectURL(blob);
     
-    const filename = encodeURIComponent(title)+'.ics';
+    const filename = encodeURIComponent(title) + '.ics';
 
     // 5. Create a hidden anchor element to trigger download
     const link = document.createElement('a');
